9ad5222458c4f553d019bd0f3d1718e5ba261227
manojchaudhary04 <manoj.chaudhary_cs18@gla.ac.in>
2020-03-28 20:18:08 +0530

    Updated Node class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,51 @@
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private Student student;
+    private Node next;
+
+    Node(Student student) {
+        this.student = student;
+        this.next = null;
+    }
+
+    public Node() {
+
+    }
+
+    public Node getNode() {
+        return next;
+    }
+
+    public void setNode(Node node) {
+        this.next = node;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void setData(int val) {
+    }
+
+    public void setNext(Node newnode) {
+    }
+
+    public void getNext() {
+    }
+
+    public void getData() {
+    }
 }
