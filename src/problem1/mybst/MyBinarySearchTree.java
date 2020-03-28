8e88a270e7b9cf98df47fc5d263538043d6757d0
manojchaudhary04 <manoj.chaudhary_cs18@gla.ac.in>
2020-03-28 18:12:47 +0530

    Updated mybst class and created fields

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,78 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+    int count;
+
+    MyBinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int val) {
+        TreeNode newnode = new TreeNode();
+        newnode.setData(val);
+        if (root == null) {
+            root = newnode;
+        } else {
+            TreeNode current = root;
+            TreeNode parent;
+            while (true) {
+                parent = current;
+                if (current.getData() > val) {
+                    current = current.getLeft();
+                    if (current == null) {
+                        parent.setLeft(newnode);
+                        return;
+                    }
+                } else {
+                    current = current.getRight();
+                    if (current == null) {
+                        parent.setRight(newnode);
+                        return;
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
+
+    void printleft(TreeNode root) {
+        if (root == null) {
+            return;
+        } else {
+
+            if (root.getLeft() != null) {
+                System.out.print(root.getLeft().getData() + " ");
+            } else {
+                count++;
+
+            }
+        }
+        printleft(root.getLeft());
+        printleft(root.getRight());
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void postorder(TreeNode root) {
+        if (root == null) {
+            return;
+        } else {
+            postorder(root.getLeft());
+            postorder(root.getRight());
+            System.out.print(root.getData() + " ");
+
+
+        }
+    }
 }
