328f663292a7aa7fc2e496e62f5abbb7d75a848d
manojchaudhary04 <manoj.chaudhary_cs18@gla.ac.in>
2020-03-28 20:34:58 +0530

    Updated MyMain class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -26,6 +26,5 @@ public class MyMain {
         obj.postOrder(obj.root);
         System.out.println("\nFirst statement is true");
         System.out.println("Second statement is False as we didn't get the middle point same.");
-
     }
 }
